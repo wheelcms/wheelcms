edc961c104edc210fb64af83d1a8356af7470029
Ivo van der Wijk <github@in.m3r.nl>
2013-09-15 11:24:28 +0200

    make it wheelcms specific

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ version = '0.8.0'
 setup(name='wheelcms',
       version=version,
       description="Overall package for WheelCMS",
-      long_description=open("README.txt").read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description=open("README.md").read(),
       classifiers=[
         "Programming Language :: Python",
         ],
       keywords='',
       author='Ivo van der Wijk',
-      author_email='two@in.m3r.nl',
-      url='http://github.com/iivvoo/two.test',
+      author_email='wheelcms@in.m3r.nl',
+      url='http://github.com/wheelcms/wheelcms',
       license='BSD',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=[],
